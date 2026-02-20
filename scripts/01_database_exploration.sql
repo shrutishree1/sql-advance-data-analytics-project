9c0eb2c3808e627b6c746771adad42ca247a89c0
Shruti Shree <92637277+shrutishree1@users.noreply.github.com>
2026-02-20 19:16:36 +0530

    Create 01_database_exploration.sql

---
+++ scripts/01_database_exploration.sql
@@ -0,0 +1,27 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
+
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
+-- EXPLORE ALL OBJECTS IN THE DATABASE
+SELECT 
+* 
+FROM INFORMATION_SCHEMA.TABLES
+
+-- EXPLORE ALL COLUMNS IN THE DATABASE
+SELECT 
+* 
+FROM INFORMATION_SCHEMA.COLUMNS
+
+-- EXPLORE ALL COLUMNS IN A SPECIFIC TABLE (dim_customers)
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers'
